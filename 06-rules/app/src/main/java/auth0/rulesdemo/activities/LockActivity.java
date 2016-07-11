7dd92f61195f09435f79cde4f3916ddfafe8dc2c
Emiliano <emiliano@inakanetworks.com>
2016-07-11 16:13:28 -0300

    Add 06 Rules Demo Project

---
+++ 06-rules/app/src/main/java/auth0/rulesdemo/activities/LockActivity.java
@@ -0,0 +1,69 @@
+package auth0.rulesdemo.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.auth0.Auth0;
+import com.auth0.android.lock.AuthenticationCallback;
+import com.auth0.android.lock.Lock;
+import com.auth0.android.lock.LockCallback;
+import com.auth0.android.lock.utils.LockException;
+import com.auth0.authentication.result.Credentials;
+
+import auth0.rulesdemo.R;
+import auth0.rulesdemo.application.App;
+
+
+/**
+ * Created by emi on 6/6/16.
+ */
+
+public class LockActivity extends Activity{
+
+        private Lock lock;
+
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+
+            Auth0 auth0 = new Auth0(getString(R.string.auth0_client_id), getString(R.string.auth0_domain));
+            this.lock = Lock.newBuilder(auth0, callback)
+                    //Add parameters to the build
+                    .build();
+            lock.onCreate(this);
+            auth0.getAuthorizeUrl();
+            startActivity(this.lock.newIntent(this));
+        }
+
+        @Override
+        protected void onDestroy() {
+            super.onDestroy();
+            // Your own Activity code
+            lock.onDestroy(this);
+            lock = null;
+        }
+
+        private LockCallback callback = new AuthenticationCallback() {
+            @Override
+            public void onAuthentication(Credentials credentials) {
+                Toast.makeText(getApplicationContext(), "Log In - Success", Toast.LENGTH_SHORT).show();
+                App.getInstance().setUserCredentials(credentials);
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+
+            @Override
+            public void onCanceled() {
+                Toast.makeText(getApplicationContext(), "Log In - Cancelled", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(LockException error){
+                Toast.makeText(getApplicationContext(), "Log In - Error Occurred", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+}
+
+
