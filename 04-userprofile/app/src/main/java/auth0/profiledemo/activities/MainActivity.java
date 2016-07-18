e00b45bd57e9dad1665e235328fa797b4e8e5964
Emiliano <emiliano@inakanetworks.com>
2016-07-18 19:12:56 -0300

    UI Update

--- 04-userprofile/app/src/main/java/auth0/profiledemo/activities/MainActivity.java
+++ 04-userprofile/app/src/main/java/auth0/profiledemo/activities/MainActivity.java
@@ -2,8 +2,10 @@ package auth0.profiledemo.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.auth0.Auth0;
 import com.auth0.Auth0Exception;
@@ -18,11 +20,20 @@ import auth0.profiledemo.application.App;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mEditProfileButton;
+    private Button mCancelEditionButton;
+    private EditText mUsernameEditext;
+    private EditText mUsermailEditext;
+    private UserProfile mUserProfile;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        // The process to reclaim an UserProfile is preceded by an Authentication call.
         AuthenticationAPIClient client = new AuthenticationAPIClient(new Auth0(getString(R.string.auth0_client_id), getString(R.string.auth0_domain)));
         client.tokenInfo(App.getInstance().getUserCredentials().getIdToken())
                 .start(new BaseCallback<UserProfile>() {
@@ -30,8 +41,9 @@ public class MainActivity extends AppCompatActivity {
                     public void onSuccess(final UserProfile payload) {
                         MainActivity.this.runOnUiThread(new Runnable() {
                             public void run() {
-                                ((TextView)findViewById(R.id.username)).setText(payload.getName());
-                                ((TextView)findViewById(R.id.usermail)).setText(payload.getEmail());
+                                mUserProfile = payload;
+                                mUsernameEditext.setText(mUserProfile.getName());
+                                mUsermailEditext.setText(mUserProfile.getEmail());
                                 ImageView userPicture = (ImageView)findViewById(R.id.userPicture);
                                 Picasso.with(getApplicationContext()).load(payload.getPictureURL()).into(userPicture);
                             }
@@ -43,5 +55,57 @@ public class MainActivity extends AppCompatActivity {
                     public void onFailure(Auth0Exception error) { }
                 });
 
+
+        mEditProfileButton = (Button) findViewById(R.id.editButton);
+        mCancelEditionButton = (Button) findViewById(R.id.cancelEditionButton);
+        mUsermailEditext = (EditText) findViewById(R.id.usermailEditext);
+        mUsernameEditext = (EditText) findViewById(R.id.usernameEditext);
+
+
+
+        mEditProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editProfile();
+            }
+        });
+
+        mCancelEditionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelProfileEdition();
+            }
+        });
+
+    }
+
+    private void editProfile() {
+        if(!mUsermailEditext.isEnabled()){
+        editModeOn(true);
+        }
+        else{
+        editModeOn(false);
+        }
+    }
+
+    private void editModeOn(boolean editModeOn) {
+        if(editModeOn){
+            mUsermailEditext.setEnabled(true);
+            mUsernameEditext.setEnabled(true);
+            mEditProfileButton.setText(getString(R.string.save));
+            mCancelEditionButton.setVisibility(View.VISIBLE);
+        }
+        else {
+            mUsermailEditext.setEnabled(false);
+            mUsernameEditext.setEnabled(false);
+            mEditProfileButton.setText(getString(R.string.edit));
+            mCancelEditionButton.setVisibility(View.GONE);
+        }
+    }
+
+    private void cancelProfileEdition() {
+        mUsernameEditext.setText(mUserProfile.getName());
+        mUsermailEditext.setText(mUserProfile.getEmail());
+        editModeOn(false);
     }
 }
