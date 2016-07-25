2c3977ccd34a271409978e99ef9f9ac349b233a4
Emiliano <emiliano@inakanetworks.com>
2016-07-25 15:57:14 -0300

    Create Seed Project

--- 00-seed-project/app/src/main/java/auth0/seedproject/activities/StartActivity.java
+++ 00-seed-project/app/src/main/java/auth0/seedproject/activities/StartActivity.java
@@ -1,10 +1,9 @@
-package auth0.callyourapidemo.activities;
+package auth0.seedproject.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import auth0.callyourapidemo.R;
+import auth0.seedproject.R;
 
 
 public class StartActivity extends AppCompatActivity {
@@ -13,10 +12,6 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-
-        Intent lockIntent = new Intent(this, LockActivity.class);
-        startActivity(lockIntent);
-
     }
 
 
