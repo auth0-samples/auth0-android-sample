7964c3db846d1825bfaac1e91e452909c56c1f8b
Luciano Balmaceda <balmacedaluciano@gmail.com>
2017-03-10 11:39:54 -0300

    allow to log in using web auth provider

--- 02-Custom-Login/app/src/main/java/com/auth0/customlogindemo/LoginActivity.java
+++ 02-Custom-Login/app/src/main/java/com/auth0/customlogindemo/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.auth0.customlogindemo;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,12 +13,13 @@ import com.auth0.android.Auth0;
 import com.auth0.android.authentication.AuthenticationAPIClient;
 import com.auth0.android.authentication.AuthenticationException;
 import com.auth0.android.callback.BaseCallback;
+import com.auth0.android.provider.AuthCallback;
+import com.auth0.android.provider.WebAuthProvider;
 import com.auth0.android.result.Credentials;
 
 
 public class LoginActivity extends Activity {
 
-    private static final String DEFAULT_DB_CONNECTION = "Username-Password-Authentication";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,25 +28,42 @@ public class LoginActivity extends Activity {
         // Bind your views
         final EditText emailEditText = (EditText) findViewById(R.id.emailEditext);
         final EditText passwordEditText = (EditText) findViewById(R.id.passwordEditext);
-        Button loginButton = (Button) findViewById(R.id.loginButton);
+        Button dbLoginButton = (Button) findViewById(R.id.dbLoginButton);
+        Button webLoginButton = (Button) findViewById(R.id.webLoginButton);
 
-        // Add the onClick listener to the login
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        // Add the onClick listener to the database login
+        dbLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Show a progress dialog to block the UI while the request is being made.
                 login(emailEditText.getText().toString(), passwordEditText.getText().toString());
             }
         });
+        // Add the onClick listener to the web auth login
+        webLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Show a progress dialog to block the UI while the request is being made.
+                login();
+            }
+        });
+    }
 
-
+    @Override
+    protected void onNewIntent(Intent intent) {
+        //Check if the result belongs to a pending web authentication
+        if (WebAuthProvider.resume(intent)) {
+            return;
+        }
+        super.onNewIntent(intent);
     }
 
     private void login(String email, String password) {
         Auth0 auth0 = new Auth0(getString(R.string.auth0_client_id), getString(R.string.auth0_domain));
         AuthenticationAPIClient client = new AuthenticationAPIClient(auth0);
 
-        client.login(email, password, DEFAULT_DB_CONNECTION).start(new BaseCallback<Credentials, AuthenticationException>() {
+        String connectionName = "Username-Password-Authentication";
+        client.login(email, password, connectionName).start(new BaseCallback<Credentials, AuthenticationException>() {
             @Override
             public void onSuccess(Credentials payload) {
                 startActivity(new Intent(LoginActivity.this, MainActivity.class));
@@ -54,6 +74,28 @@ public class LoginActivity extends Activity {
                 //Show error to the user
             }
         });
+    }
+
+    private void login() {
+        Auth0 auth0 = new Auth0(getString(R.string.auth0_client_id), getString(R.string.auth0_domain));
+        WebAuthProvider.init(auth0)
+                .withConnection("twitter")
+                .start(LoginActivity.this, new AuthCallback() {
+                    @Override
+                    public void onFailure(@NonNull Dialog dialog) {
+                        dialog.show();
+                    }
+
+                    @Override
+                    public void onFailure(AuthenticationException exception) {
+                        //Show error to the user
+                    }
+
+                    @Override
+                    public void onSuccess(@NonNull Credentials credentials) {
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    }
+                });
     }
 }
 
