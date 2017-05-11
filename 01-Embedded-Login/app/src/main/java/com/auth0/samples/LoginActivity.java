939c83d780f28ceb2e477cc4414a8466e3e6b1d1
Luciano Balmaceda <balmacedaluciano@gmail.com>
2017-05-11 17:37:22 -0300

    update embedded-login

--- 01-Embedded-Login/app/src/main/java/com/auth0/samples/LoginActivity.java
+++ 01-Embedded-Login/app/src/main/java/com/auth0/samples/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.auth0.logindemo;
+package com.auth0.samples;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -22,6 +22,7 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         Auth0 auth0 = new Auth0(getString(R.string.auth0_client_id), getString(R.string.auth0_domain));
         mLock = Lock.newBuilder(auth0, mCallback)
+                .withScheme("demo")
                 //Add parameters to the builder
                 .build(this);
         startActivity(mLock.newIntent(this));
