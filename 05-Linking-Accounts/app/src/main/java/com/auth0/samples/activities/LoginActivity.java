6d895f20009c8d9bca03672cd123930a1f4eda39
Luciano Balmaceda <balmacedaluciano@gmail.com>
2017-05-12 16:49:05 -0300

    update linking-accounts

---
+++ 05-Linking-Accounts/app/src/main/java/com/auth0/samples/activities/LoginActivity.java
@@ -0,0 +1,145 @@
+package com.auth0.samples.activities;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.auth0.android.Auth0;
+import com.auth0.android.authentication.AuthenticationException;
+import com.auth0.android.callback.BaseCallback;
+import com.auth0.android.management.ManagementException;
+import com.auth0.android.management.UsersAPIClient;
+import com.auth0.android.provider.AuthCallback;
+import com.auth0.android.provider.WebAuthProvider;
+import com.auth0.android.result.Credentials;
+import com.auth0.android.result.UserIdentity;
+import com.auth0.samples.R;
+import com.auth0.samples.utils.Constants;
+import com.auth0.samples.utils.CredentialsManager;
+
+import java.util.List;
+
+
+public class LoginActivity extends Activity {
+
+    private Auth0 auth0;
+    private String primaryUserId;
+    private boolean linkSessions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        auth0 = new Auth0(getString(R.string.auth0_client_id), getString(R.string.auth0_domain));
+        auth0.setOIDCConformant(true);
+
+        if (getIntent().getExtras() != null) {
+            linkSessions = getIntent().getExtras().getBoolean(Constants.LINK_ACCOUNTS, false);
+            primaryUserId = getIntent().getExtras().getString(Constants.PRIMARY_USER_ID);
+            doLogin();
+            return;
+        }
+
+        setContentView(R.layout.activity_login);
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doLogin();
+            }
+        });
+    }
+
+    private void doLogin() {
+        WebAuthProvider.init(auth0)
+                .withScheme("demo")
+                .start(this, callback);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        if (WebAuthProvider.resume(intent)) {
+            return;
+        }
+        super.onNewIntent(intent);
+    }
+
+    private final AuthCallback callback = new AuthCallback() {
+        @Override
+        public void onFailure(@NonNull final Dialog dialog) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    dialog.show();
+                }
+            });
+            if (linkSessions) {
+                finish();
+            }
+        }
+
+        @Override
+        public void onFailure(AuthenticationException exception) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(LoginActivity.this, "Log In - Error Occurred", Toast.LENGTH_SHORT).show();
+                }
+            });
+            if (linkSessions) {
+                finish();
+            }
+        }
+
+        @Override
+        public void onSuccess(@NonNull Credentials credentials) {
+            if (linkSessions) {
+                performLink(credentials.getIdToken());
+                return;
+            }
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(LoginActivity.this, "Log In - Success", Toast.LENGTH_SHORT).show();
+                }
+            });
+            CredentialsManager.saveCredentials(LoginActivity.this, credentials);
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
+    };
+
+    private void performLink(String secondaryIdToken) {
+        UsersAPIClient client = new UsersAPIClient(auth0, CredentialsManager.getCredentials(LoginActivity.this).getIdToken());
+        client.link(primaryUserId, secondaryIdToken)
+                .start(new BaseCallback<List<UserIdentity>, ManagementException>() {
+                    @Override
+                    public void onSuccess(List<UserIdentity> payload) {
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+                                Toast.makeText(LoginActivity.this, "Accounts linked!", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        finish();
+                    }
+
+                    @Override
+                    public void onFailure(ManagementException error) {
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+                                Toast.makeText(LoginActivity.this, "Account linking failed!", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        finish();
+                    }
+                });
+    }
+
+}
+
+
--- 05-Linking-Accounts/app/src/main/java/com/auth0/samples/activities/MainActivity.java
+++ 05-Linking-Accounts/app/src/main/java/com/auth0/samples/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.auth0.linkingaccountsdemo.activities;
+package com.auth0.samples.activities;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -22,9 +22,9 @@ import com.auth0.android.management.ManagementException;
 import com.auth0.android.management.UsersAPIClient;
 import com.auth0.android.result.UserIdentity;
 import com.auth0.android.result.UserProfile;
-import com.auth0.linkingaccountsdemo.R;
-import com.auth0.linkingaccountsdemo.utils.Constants;
-import com.auth0.linkingaccountsdemo.utils.CredentialsManager;
+import com.auth0.samples.R;
+import com.auth0.samples.utils.Constants;
+import com.auth0.samples.utils.CredentialsManager;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -33,12 +33,12 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mUserEmailTextView;
-    private ImageView mUserPicture;
-    private UserProfile mUserProfile;
-    private Auth0 mAuth0;
-    private UsersAPIClient mUsersClient;
-    private ListView mLinkedAccountList;
+    private Auth0 auth0;
+    private UsersAPIClient usersClient;
+    private TextView userEmailTextView;
+    private ImageView userPicture;
+    private UserProfile userProfile;
+    private ListView linkedAccountList;
 
 
     @Override
@@ -46,11 +46,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAuth0 = new Auth0(getString(R.string.auth0_client_id), getString(R.string.auth0_domain));
-        mUsersClient = new UsersAPIClient(mAuth0, CredentialsManager.getCredentials(MainActivity.this).getIdToken());
+        auth0 = new Auth0(getString(R.string.auth0_client_id), getString(R.string.auth0_domain));
+        auth0.setOIDCConformant(true);
+        usersClient = new UsersAPIClient(auth0, CredentialsManager.getCredentials(MainActivity.this).getIdToken());
 
-        mUserEmailTextView = (TextView) findViewById(R.id.userEmailTitle);
-        mUserPicture = (ImageView) findViewById(R.id.userPicture);
+        userEmailTextView = (TextView) findViewById(R.id.userEmailTitle);
+        userPicture = (ImageView) findViewById(R.id.userPicture);
 
         Button linkAccountButton = (Button) findViewById(R.id.linkAccountButton);
         linkAccountButton.setOnClickListener(new View.OnClickListener() {
@@ -68,11 +69,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mLinkedAccountList = (ListView) findViewById(R.id.linkedAccountsList);
-        mLinkedAccountList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        linkedAccountList = (ListView) findViewById(R.id.linkedAccountsList);
+        linkedAccountList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                final UserIdentity identity = mUserProfile.getIdentities().get(position);
+                final UserIdentity identity = userProfile.getIdentities().get(position);
                 if (isPrimaryIdentity(identity)) {
                     Toast.makeText(MainActivity.this, "You cannot unlink primary account", Toast.LENGTH_SHORT).show();
                     return;
@@ -99,12 +100,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void fetchProfileInfo() {
         // The process to reclaim User Information is preceded by an Authentication call.
-        AuthenticationAPIClient authenticationClient = new AuthenticationAPIClient(mAuth0);
-        authenticationClient.tokenInfo(CredentialsManager.getCredentials(MainActivity.this).getIdToken())
+        AuthenticationAPIClient authenticationClient = new AuthenticationAPIClient(auth0);
+        authenticationClient.userInfo(CredentialsManager.getCredentials(MainActivity.this).getAccessToken())
                 .start(new BaseCallback<UserProfile, AuthenticationException>() {
                     @Override
                     public void onSuccess(UserProfile profile) {
-                        mUserProfile = profile;
+                        userProfile = profile;
                         runOnUiThread(new Runnable() {
                             public void run() {
                                 refreshScreenInformation();
@@ -124,34 +125,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private boolean isPrimaryIdentity(UserIdentity identity) {
-        return identity.equals(mUserProfile.getIdentities().get(0));
+        return identity.equals(userProfile.getIdentities().get(0));
     }
 
     private void refreshScreenInformation() {
-        mUserEmailTextView.setText(String.format(getString(R.string.userEmail), mUserProfile.getEmail()));
-        Picasso.with(getApplicationContext()).load(mUserProfile.getPictureURL()).into(mUserPicture);
-
-        if (mUserProfile.getIdentities() == null) {
+        userEmailTextView.setText(String.format(getString(R.string.userEmail), userProfile.getEmail()));
+        Picasso.with(this).load(userProfile.getPictureURL()).into(userPicture);
+        if (userProfile.getIdentities() == null) {
             return;
         }
 
         List<String> identitiesTypes = new ArrayList<>();
-        for (UserIdentity identity : mUserProfile.getIdentities()) {
+        for (UserIdentity identity : userProfile.getIdentities()) {
             identitiesTypes.add(identity.getConnection());
         }
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, identitiesTypes);
-        mLinkedAccountList.setAdapter(adapter);
+        linkedAccountList.setAdapter(adapter);
     }
 
     private void linkAccount() {
-        Intent lockToLinkAccounts = new Intent(this, LoginActivity.class);
-        lockToLinkAccounts.putExtra(Constants.LINK_ACCOUNTS, true);
-        lockToLinkAccounts.putExtra(Constants.PRIMARY_USER_ID, mUserProfile.getId());
-        startActivity(lockToLinkAccounts);
+        Intent linkAccounts = new Intent(this, LoginActivity.class);
+        linkAccounts.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        linkAccounts.putExtra(Constants.LINK_ACCOUNTS, true);
+        linkAccounts.putExtra(Constants.PRIMARY_USER_ID, userProfile.getId());
+        startActivity(linkAccounts);
     }
 
     private void unlink(UserIdentity secondaryAccountIdentity) {
-        mUsersClient.unlink(mUserProfile.getId(), secondaryAccountIdentity.getId(), secondaryAccountIdentity.getProvider())
+        usersClient.unlink(userProfile.getId(), secondaryAccountIdentity.getId(), secondaryAccountIdentity.getProvider())
                 .start(new BaseCallback<List<UserIdentity>, ManagementException>() {
                     @Override
                     public void onSuccess(List<UserIdentity> payload) {
@@ -175,6 +176,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loginAgain() {
+        CredentialsManager.deleteCredentials(this);
         startActivity(new Intent(this, LoginActivity.class));
         finish();
     }
