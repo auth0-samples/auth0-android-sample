a6d2d61d7234511df17f617edb361de21cbf9174
Luciano Balmaceda <balmacedaluciano@gmail.com>
2017-06-08 12:38:05 -0300

    add missing embedded-login sample

---
+++ 01-Embedded-Login/app/src/main/java/com/auth0/samples/MainActivity.java
@@ -0,0 +1,30 @@
+package com.auth0.samples;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button loginAgainButton = (Button) findViewById(R.id.login_again);
+        loginAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginAgain();
+            }
+        });
+    }
+
+    private void loginAgain() {
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
+}
