0c5b7b7d64c120c0dff32dadb3b13928ddcfae94
Alexis Luque <aluque@indexlabs.com.uy>
2019-09-13 20:03:39 -0300

    Add tenant logout level to login and calling APIs chapters

--- 00-Login/app/src/main/java/com/auth0/samples/MainActivity.java
+++ 00-Login/app/src/main/java/com/auth0/samples/MainActivity.java
@@ -1,20 +1,13 @@
 package com.auth0.samples;
 
 import android.app.Activity;
-import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.auth0.android.Auth0;
-import com.auth0.android.authentication.AuthenticationException;
-import com.auth0.android.provider.AuthCallback;
-import com.auth0.android.provider.WebAuthProvider;
-import com.auth0.android.result.Credentials;
 
 /**
  * Created by lbalmaceda on 5/10/17.
@@ -22,61 +15,30 @@ import com.auth0.android.result.Credentials;
 
 public class MainActivity extends Activity {
 
-    private TextView token;
     private Auth0 auth0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        token = findViewById(R.id.token);
-        Button loginButton = findViewById(R.id.loginButton);
+        Button loginButton = findViewById(R.id.logout);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                login();
+                logout();
             }
         });
-        auth0 = new Auth0(this);
-        auth0.setOIDCConformant(true);
 
+        //Obtain the token from the Intent's extras
+        String accessToken = getIntent().getStringExtra(LoginActivity.EXTRA_ACCESS_TOKEN);
+        TextView textView = findViewById(R.id.credentials);
+        textView.setText(accessToken);
     }
 
-    private void login() {
-        token.setText("Not logged in");
-        WebAuthProvider.login(auth0)
-                .withScheme("demo")
-                .withAudience(String.format("https://%s/userinfo", getString(R.string.com_auth0_domain)))
-                .start(MainActivity.this, new AuthCallback() {
-                    @Override
-                    public void onFailure(@NonNull final Dialog dialog) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                dialog.show();
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void onFailure(final AuthenticationException exception) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(MainActivity.this, "Error: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void onSuccess(@NonNull final Credentials credentials) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                token.setText("Logged in: " + credentials.getAccessToken());
-                            }
-                        });
-                    }
-                });
+    private void logout() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.putExtra(LoginActivity.EXTRA_CLEAR_CREDENTIALS, true);
+        startActivity(intent);
+        finish();
     }
 }
