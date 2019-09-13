d0a44247ee3eb95aab1c370250bc4e3d9101ebbe
Alexis Luque <aluque@indexlabs.com.uy>
2019-09-13 20:03:39 -0300

    Add tenant logout level to login and calling APIs chapters

---
+++ 00-Login/app/src/main/java/com/auth0/samples/LoginActivity.java
@@ -0,0 +1,108 @@
+package com.auth0.samples;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import com.auth0.android.Auth0;
+import com.auth0.android.Auth0Exception;
+import com.auth0.android.authentication.AuthenticationException;
+import com.auth0.android.provider.AuthCallback;
+import com.auth0.android.provider.VoidCallback;
+import com.auth0.android.provider.WebAuthProvider;
+import com.auth0.android.result.Credentials;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Auth0 auth0;
+
+    public static final String EXTRA_CLEAR_CREDENTIALS = "com.auth0.CLEAR_CREDENTIALS";
+    public static final String EXTRA_ACCESS_TOKEN = "com.auth0.ACCESS_TOKEN";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+        auth0 = new Auth0(this);
+        auth0.setOIDCConformant(true);
+
+        //Check if the activity was launched to log the user out
+        if (getIntent().getBooleanExtra(EXTRA_CLEAR_CREDENTIALS, false)) {
+            logout();
+        }
+    }
+
+    private void login() {
+        WebAuthProvider.login(auth0)
+                .withScheme("demo")
+                .withAudience(String.format("https://%s/userinfo", getString(R.string.com_auth0_domain)))
+                .start(this, new AuthCallback() {
+                    @Override
+                    public void onFailure(@NonNull final Dialog dialog) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                dialog.show();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(final AuthenticationException exception) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(LoginActivity.this, "Error: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onSuccess(@NonNull final Credentials credentials) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                intent.putExtra(EXTRA_ACCESS_TOKEN, credentials.getAccessToken());
+                                startActivity(intent);
+                                finish();
+                            }
+                        });
+                    }
+                });
+    }
+
+    private void logout() {
+        WebAuthProvider.logout(auth0)
+                .withScheme("demo")
+                .start(this, new VoidCallback() {
+                    @Override
+                    public void onSuccess(Void payload) {
+
+                    }
+
+                    @Override
+                    public void onFailure(Auth0Exception error) {
+                        //Log out canceled, keep the user logged in
+                        showNextActivity();
+                    }
+                });
+    }
+
+    private void showNextActivity() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
--- 06-Calling-APIs/app/src/main/java/com/auth0/samples/LoginActivity.java
+++ 06-Calling-APIs/app/src/main/java/com/auth0/samples/LoginActivity.java
@@ -2,24 +2,24 @@ package com.auth0.samples;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import com.auth0.android.Auth0;
+import com.auth0.android.Auth0Exception;
+import com.auth0.android.authentication.AuthenticationAPIClient;
 import com.auth0.android.authentication.AuthenticationException;
+import com.auth0.android.authentication.storage.CredentialsManagerException;
+import com.auth0.android.authentication.storage.SecureCredentialsManager;
+import com.auth0.android.authentication.storage.SharedPreferencesStorage;
+import com.auth0.android.callback.BaseCallback;
 import com.auth0.android.provider.AuthCallback;
+import com.auth0.android.provider.VoidCallback;
 import com.auth0.android.provider.WebAuthProvider;
 import com.auth0.android.result.Credentials;
-import com.squareup.okhttp.Callback;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
-
-import java.io.IOException;
 
 /**
  * Created by lbalmaceda on 5/10/17.
@@ -27,109 +27,137 @@ import java.io.IOException;
 
 public class LoginActivity extends Activity {
 
-    private static final String API_URL = "YOUR API URL";
+    private Auth0 auth0;
+    private SecureCredentialsManager credentialsManager;
 
-    private String accessToken;
+    /*
+     * Required when setting up Local Authentication in the Credential Manager
+     * Refer to SecureCredentialsManager#requireAuthentication method for more information.
+     */
+    @SuppressWarnings("unused")
+    private static final int CODE_DEVICE_AUTHENTICATION = 22;
+    public static final String EXTRA_CLEAR_CREDENTIALS = "com.auth0.CLEAR_CREDENTIALS";
+    public static final String EXTRA_ACCESS_TOKEN = "com.auth0.ACCESS_TOKEN";
+    public static final String EXTRA_ID_TOKEN = "com.auth0.ID_TOKEN";
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity);
-        Button callAPIWithTokenButton = findViewById(R.id.callAPIWithTokenButton);
-        Button callAPIWithoutTokenButton = findViewById(R.id.callAPIWithoutTokenButton);
-        Button loginWithTokenButton = findViewById(R.id.loginButton);
-        callAPIWithTokenButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                callAPI(true);
-            }
-        });
-        callAPIWithoutTokenButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                callAPI(false);
-            }
-        });
-        loginWithTokenButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                login();
-            }
-        });
-    }
 
-    private void callAPI(boolean sendToken) {
-        final Request.Builder reqBuilder = new Request.Builder()
-                .get()
-                .url(API_URL);
-        if (sendToken) {
-            if (accessToken == null) {
-                Toast.makeText(LoginActivity.this, "Token not found. Log in first.", Toast.LENGTH_SHORT).show();
-                return;
+        //Setup the UI
+        setContentView(R.layout.login_activity);
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doLogin();
             }
-            reqBuilder.addHeader("Authorization", "Bearer " + accessToken);
+        });
+
+        //Setup CredentialsManager
+        auth0 = new Auth0(this);
+        auth0.setOIDCConformant(true);
+        credentialsManager = new SecureCredentialsManager(this, new AuthenticationAPIClient(auth0), new SharedPreferencesStorage(this));
+
+        //Optional - Uncomment the next line to use:
+        //Require device authentication before obtaining the credentials
+        //credentialsManager.requireAuthentication(this, CODE_DEVICE_AUTHENTICATION, getString(R.string.request_credentials_title), null);
+
+        //Check if the activity was launched to log the user out
+        if (getIntent().getBooleanExtra(EXTRA_CLEAR_CREDENTIALS, false)) {
+            doLogout();
+            return;
         }
 
-        OkHttpClient client = new OkHttpClient();
-        Request request = reqBuilder.build();
-        client.newCall(request).enqueue(new Callback() {
+        if (credentialsManager.hasValidCredentials()) {
+            // Obtain the existing credentials and move to the next activity
+            showNextActivity();
+        }
+    }
+
+    /**
+     * Override required when setting up Local Authentication in the Credential Manager
+     * Refer to SecureCredentialsManager#requireAuthentication method for more information.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (credentialsManager.checkAuthenticationResult(requestCode, resultCode)) {
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void showNextActivity() {
+        credentialsManager.getCredentials(new BaseCallback<Credentials, CredentialsManagerException>() {
             @Override
-            public void onFailure(Request request, IOException e) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(LoginActivity.this, "An error occurred", Toast.LENGTH_SHORT).show();
-                    }
-                });
+            public void onSuccess(final Credentials credentials) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.putExtra(EXTRA_ACCESS_TOKEN, credentials.getAccessToken());
+                intent.putExtra(EXTRA_ID_TOKEN, credentials.getIdToken());
+                startActivity(intent);
+                finish();
             }
 
             @Override
-            public void onResponse(final Response response) throws IOException {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (response.isSuccessful()) {
-                            Toast.makeText(LoginActivity.this, "API call success!", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(LoginActivity.this, "API call failed.", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+            public void onFailure(CredentialsManagerException error) {
+                //Authentication cancelled by the user. Exit the app
+                finish();
             }
         });
     }
 
-    private void login() {
-        Auth0 auth0 = new Auth0(this);
-        auth0.setOIDCConformant(true);
+    private void doLogin() {
         WebAuthProvider.login(auth0)
                 .withScheme("demo")
                 .withAudience(String.format("https://%s/userinfo", getString(R.string.com_auth0_domain)))
-                .start(LoginActivity.this, new AuthCallback() {
-                    @Override
-                    public void onFailure(@NonNull final Dialog dialog) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                dialog.show();
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void onFailure(final AuthenticationException exception) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(LoginActivity.this, "Error: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void onSuccess(@NonNull final Credentials credentials) {
-                        accessToken = credentials.getAccessToken();
-                    }
-                });
+                .withScope("openid offline_access")
+                .start(this, loginCallback);
     }
+
+    private void doLogout() {
+        WebAuthProvider.logout(auth0)
+                .withScheme("demo")
+                .start(this, logoutCallback);
+    }
+
+    private final AuthCallback loginCallback = new AuthCallback() {
+        @Override
+        public void onFailure(@NonNull final Dialog dialog) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    dialog.show();
+                }
+            });
+        }
+
+        @Override
+        public void onFailure(final AuthenticationException exception) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(LoginActivity.this, "Log In - Error Occurred", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public void onSuccess(@NonNull Credentials credentials) {
+            credentialsManager.saveCredentials(credentials);
+            showNextActivity();
+        }
+    };
+
+    private VoidCallback logoutCallback = new VoidCallback() {
+        @Override
+        public void onSuccess(Void payload) {
+            credentialsManager.clearCredentials();
+        }
+
+        @Override
+        public void onFailure(Auth0Exception error) {
+            //Log out canceled, keep the user logged in
+            showNextActivity();
+        }
+    };
 }
