614d05ce809b468f47f68864384aecce4edb0652
Luciano Balmaceda <balmacedaluciano@gmail.com>
2021-01-19 22:16:09 +0100

    update kotlin and java login samples with v2-beta release

--- 00-Login/app/src/main/java/com/auth0/samples/MainActivity.java
+++ 00-Login/app/src/main/java/com/auth0/samples/MainActivity.java
@@ -3,19 +3,17 @@ package com.auth0.samples;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import com.auth0.android.Auth0;
+
+import androidx.annotation.Nullable;
 
 /**
  * Created by lbalmaceda on 5/10/17.
  */
 
 public class MainActivity extends Activity {
-
-    private Auth0 auth0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
